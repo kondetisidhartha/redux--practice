9ae62bcac1c623fa480da9823c5a6400e60f8f75
Sidhartha <kondetisidhartha@gmail.com>
2020-04-20 00:09:36 +0200

    thunk coded

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import { increment, decrement, add10, subtract5, onStoreResult, onDeleteResult } from '../../store/actions'
+import { increment, decrement, add10, subtract5, onStoreResult, onDeleteResult } from '../../store/actions/index'
 // import * as actionCreators from '../../store/actions'
 
 class Counter extends Component {
