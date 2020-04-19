716a9f1e0b13024a8b559f6e62d0d3c05a630cda
Sidhartha <kondetisidhartha@gmail.com>
2020-04-19 21:32:06 +0200

    Installed and compiled Redux tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
@@ -13,7 +14,21 @@ const rootReducer = combineReducers({
   ctr: counterReducer,
   result: resultReducer
 });
-const store = createStore(rootReducer);
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatch', action);
+      const result = next(action);
+      console.log('[Middleware] Next State', store.getState());
+      return result
+    }
+  }
+}
+
+const composeEnhancers = (process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
